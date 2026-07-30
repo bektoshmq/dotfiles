3f7cc78c2ababf598435dccffe64426d1d0bd8fb
Bektosh Madaminov <bektosh.mq@gmail.com>
2026-07-30 15:08:35 +0500

    herdr: add tmux-style workflow and navigation

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,18 +1,17 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
   },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  {
+    "paulbkim-dev/vim-herdr-navigation",
+    dependencies = { "christoomey/vim-tmux-navigator" },
+    lazy = false,
+    config = function(plugin)
+      dofile(plugin.dir .. "/editor/nvim.lua")
+    end,
   },
 }
